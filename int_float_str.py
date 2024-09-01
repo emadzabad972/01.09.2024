687fc225f1607d643bf3f98e839f40be4f5ce478
emadzabad972 <emadzabad972@gmail.com>
2024-09-01 20:49:52 +0300

    Add files via upload

---
+++ int_float_str.py
@@ -0,0 +1,11 @@
+# start
+
+weight: float = float(input("whats your weight?"))
+
+print(weight - 10)
+
+year_of_birth: int = int(input("what is year of birth?"))
+
+print(year_of_birth + 10)
+
+# end
