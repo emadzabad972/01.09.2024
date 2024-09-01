0c5b2d036d04ce5e640dfea7fd40a62da99df31c
emadzabad972 <emadzabad972@gmail.com>
2024-09-01 18:01:10 +0300

    Add files via upload

---
+++ HWPART1.py
@@ -0,0 +1,11 @@
+# start
+
+x: int = input("write a random number :")
+
+print(x)
+
+print(x)
+
+print(x)
+
+# end
---
+++ HWPART2.py
@@ -0,0 +1,13 @@
+# start
+
+x: int = 777
+
+y: int = 888
+
+z: int = 999
+
+mul: int = x*y+z
+
+print(mul)
+
+# end
